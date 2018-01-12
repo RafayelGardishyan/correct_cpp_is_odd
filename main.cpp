7ebf11d94fa572467bd30b35e1e83dd68b9c1551
RafayelGardishyan <rgardishyan@gmail.com>
2018-01-12 19:29:55 +0100

    Bug fix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
-main(argc, argv)
-{
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+int main(int argc, char* argv[])
+{   if (argc == 1) return 1;
+    if (argv[1]){
+        int odd = atoi(argv[1]) / 2;
+        if(odd == 0){
+            cout << "false" << "\n";
+        }
+        else{
+            cout << "true" << "\n";
+        }
+    }
+    return 0;
 }
