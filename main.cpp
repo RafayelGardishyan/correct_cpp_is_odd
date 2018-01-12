f8eae142a625c1d098e77a5f9438c356926108b3
RafayelGardishyan <rgardishyan@gmail.com>
2018-01-12 20:21:43 +0100

    Added documentations

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-
+///Main Function (Returns nothing, true of false)
 int main(int argc, char* argv[])
-{   if (argc == 1) return 1;
+{
+    if (argc == 1) return 1;
+    ///Check if there is a first argument
     if (argv[1]){
-        int odd = atoi(argv[1]) / 2;
-        if(odd == 0){
-            cout << "false" << "\n";
-        }
-        else{
-            cout << "true" << "\n";
+        ///Try to convert argument to int
+        try{
+            int odd = atoi(argv[1]) / 2;
+            ///If the integer is not odd return false
+            if(odd == 0){
+                cout << "false" << "\n";
+            }
+            ///If the integer is odd return true
+            else{
+                cout << "true" << "\n";
+            }
+        ///If the argument is not an integer don't return anything
+        }catch(invalid_argument){
+            return 1;
         }
     }
     return 0;
 }
+///<EOF>
