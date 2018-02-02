47c7bb8eeb69ad888915880c7023fb3c5f18afc7
RafayelGardishyan <rgardishyan@gmail.com>
2018-02-02 20:21:07 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-///Main Function (Returns nothing, true of false)
+
+/// RJCB: is_odd main function. is_odd needs exactly one argument, will
+/// return exit code 1 otherwise. If the argument is not an
+/// integer, or a number too big, the exit code will be 1.
 int main(int argc, char* argv[])
 {
+    //RJCB: But the user may also supply 2, 3, or more arguments. Only an argc of 2 is OK
     if (argc == 1) return 1;
+
+    //RJCB: two slashes within code, three slashes above a function/class/etc
     ///Check if there is a first argument
-    if (argv[1]){
+    //RJCB: We know there is a first argument, as argc > 1
+    //if (argv[1]){
         ///Try to convert argument to int
         try{
-            int odd = atoi(argv[1]) / 2;
+            // RJCB: An odd number uses modulo
+            int odd = atoi(argv[1]) % 2;
             ///If the integer is not odd return false
             if(odd == 0){
                 cout << "false" << "\n";
@@ -19,10 +27,11 @@ int main(int argc, char* argv[])
                 cout << "true" << "\n";
             }
         ///If the argument is not an integer don't return anything
-        }catch(invalid_argument){
+        }catch(const invalid_argument){
             return 1;
         }
-    }
+    //}
+
+    //May or may not write this. It is added by the compiler if not.
     return 0;
 }
-///<EOF>
